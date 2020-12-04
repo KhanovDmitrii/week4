c8b2c8d93a34d3e88a4c111015eecf5955f1dab5
KhanovDmitrii <khanovds@mail.ru>
2020-12-04 04:36:42 +0300

    задание4

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ var app = express();
 const NAME = "khanovds";
 const port = process.env.PORT || 80;
 
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'x-test,Content-Type,Accept,Access-Control-Allow-Headers');
+    next();
+});
+
 app.get('/', function(req, res) {
     res.send(NAME);
 });
 
 app.post('/result4/', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'x-test,Content-Type,Accept,Access-Control-Allow-Headers');
+   
     get_post(req).then(post => {
         res.send(JSON.stringify({
             "message": NAME,
